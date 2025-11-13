2bddf59bc9c4e9674175a2d8015bc8f0be922c73
Tomar Clark <ttclark.create@gmail.com>
2025-11-13 10:58:43 -0500

    questons class slightly updated, need to finish the answers arraylist and implent into level one, two and three

--- app/src/main/java/edu/bpi/quizappelectronicstc/Questions.java
+++ app/src/main/java/edu/bpi/quizappelectronicstc/Questions.java
@@ -1,6 +1,26 @@
 package edu.bpi.quizappelectronicstc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 //implement old questions
 public class Questions {
 
+    public Questions(String q, boolean a){
+        String[] questionsone = new String[]{
+                "Is a red-red-blue-gold resistor nominal value greater than 45M-ohms?",
+                "This the schematic symbol of a bulb?",
+                "Conventional Current flows from Negative(-) to Positive(+)?",
+                "Ohm's Law is represented by the equation V=(I)(R)?",
+                "Make up something here lol"};
+        String[] questionstwo = new String[]{};
+        String[] questionsthree = new String[]{};
+        ArrayList<String> answers  = new ArrayList<String>();
+
+
+    }
+
+
+    //,"false",, "true",,"false",,"true",,"false"));
 
 }
