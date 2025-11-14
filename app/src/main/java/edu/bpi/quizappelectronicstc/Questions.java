413c2271d96f2d1ca44d06dc5beae36a8cf63f1b
tomarclark8 <ttclark.create@gmail.com>
2025-11-13 19:05:02 -0500

    updated questions class, still need questions for my level two and three

--- app/src/main/java/edu/bpi/quizappelectronicstc/Questions.java
+++ app/src/main/java/edu/bpi/quizappelectronicstc/Questions.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 //implement old questions
 public class Questions {
 
-    public Questions(String q, boolean a){
+    public Questions(String q, String a){
         String[] questionsone = new String[]{
                 "Is a red-red-blue-gold resistor nominal value greater than 45M-ohms?",
                 "This the schematic symbol of a bulb?",
@@ -15,7 +15,32 @@ public class Questions {
                 "Make up something here lol"};
         String[] questionstwo = new String[]{};
         String[] questionsthree = new String[]{};
-        ArrayList<String> answers  = new ArrayList<String>();
+
+
+        ArrayList<String> answersone = new ArrayList<>(5);
+        for(int i=0; i < questionsone.length + 1; i++){
+            if(i % 2 == 0 || i == 0){
+                answersone.add("true");
+            }
+            if(i % 2 > 0){
+                answersone.add("false");
+            }
+        }
+
+        //
+        ArrayList<String> answerstwo = new ArrayList<>(3);
+        for(int i=0; i < questionstwo.length + 1; i++){
+            if(i % 2 == 0 || i == 0){
+                answersone.add("true");
+            }
+            if(i % 2 > 0){
+                answersone.add("false");
+            }
+        }
+
+        //
+        ArrayList<String> answersthree = new ArrayList<>(1);
+        answersthree.add("true");
 
 
     }
