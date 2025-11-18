8881a299aac1f23c2984f2f5432bfd0edabb97c5
Tomar Clark <ttclark.create@gmail.com>
2025-11-18 16:10:39 -0500

    just clearing and resetting formatting of questions/ level difficulty

--- app/src/main/java/edu/bpi/quizappelectronicstc/TransitionScreen.java
+++ app/src/main/java/edu/bpi/quizappelectronicstc/TransitionScreen.java
@@ -15,10 +15,7 @@ public class TransitionScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_transition_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
 }
